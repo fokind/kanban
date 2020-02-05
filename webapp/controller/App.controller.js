1b77cff54a7079061b8f7c6f0ce906636e3c80d5
Dmitry Fokin <fokind1@yandex.ru>
2020-02-05 13:48:48 +0000

    обработка драг дропа

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,5 +3,17 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("fokind.kanban.controller.App", {});
+	return Controller.extend("fokind.kanban.controller.App", {
+		onDrop: function(oEvent) {
+			console.log(oEvent.getParameters());
+			// var oDraggedContext = oEvent.getParameter("draggedControl").getBindingContext();
+			// console.log(oDraggedContext);
+			// console.log(oDraggedContext.getObject());
+			// var oDroppedContext = oEvent.getParameter("droppedControl").getBindingContext();
+			// var sDropPosition = oEvent.getParameter("dropPosition");
+			
+			// удалить из исходного
+			// добавить в целевой
+		}
+	});
 });
