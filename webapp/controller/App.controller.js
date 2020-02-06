f16c64a0b4f4cc70c81865368a491428a228ccc1
Fokin Dmitry <fokind1@yandex.ru>
2020-02-06 11:33:01 +0000

    работает драг дроп

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,16 +4,30 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("fokind.kanban.controller.App", {
-		onDrop: function(oEvent) {
-			console.log(oEvent.getParameters());
-			// var oDraggedContext = oEvent.getParameter("draggedControl").getBindingContext();
-			// console.log(oDraggedContext);
-			// console.log(oDraggedContext.getObject());
-			// var oDroppedContext = oEvent.getParameter("droppedControl").getBindingContext();
-			// var sDropPosition = oEvent.getParameter("dropPosition");
-			
+		onDrop: function (oEvent) {
+			var oModel = oEvent.getParameter("draggedControl").getBindingContext().getModel();
+			var oDraggedItem = oEvent.getParameter("draggedControl").getBindingContext().getObject();
+
 			// удалить из исходного
+			var oDraggedList = oEvent.getParameter("draggedControl").getParent().getBindingContext().getProperty("tasks");
+			var iDraggedIndex = oDraggedList.indexOf(oDraggedItem);
+			oDraggedList.splice(iDraggedIndex, 1);
+
 			// добавить в целевой
+			var sDropPosition = oEvent.getParameter("dropPosition");
+
+			if (sDropPosition === "On") {
+				var oDroppedList = oEvent.getParameter("droppedControl").getBindingContext().getProperty("tasks");
+				oDroppedList.push(oDraggedItem);
+			} else {
+				var oDroppedItem = oEvent.getParameter("droppedControl").getBindingContext().getObject();
+				var oDroppedList = oEvent.getParameter("droppedControl").getParent().getBindingContext().getProperty("tasks");
+				var iDroppedIndex = oDroppedList.indexOf(oDroppedItem) + (sDropPosition === "After" ? 1 : 0);
+				oDroppedList.splice(iDroppedIndex, 0, oDraggedItem);
+			}
+
+			// обновить модель
+			oModel.refresh();
 		}
 	});
 });
