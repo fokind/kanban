518471beff271adaa94f2399bb1aa659e1625225
Fokin Dmitry <fokind1@yandex.ru>
2020-02-06 13:08:34 +0000

    добавлен диалог редактирования

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,63 @@
 sap.ui.define([
-	'sap/ui/core/mvc/Controller'
-], function (Controller) {
+	'sap/ui/core/mvc/Controller',
+	"sap/m/Dialog",
+	"sap/m/VBox",
+	"sap/m/TextArea",
+	"sap/m/Button",
+	"sap/m/library"
+], function (Controller, Dialog, VBox, TextArea, Button, mobileLibrary) {
 	"use strict";
+	
+	var ButtonType = mobileLibrary.ButtonType;
 
 	return Controller.extend("fokind.kanban.controller.App", {
+	    _openDialog: function (sSummary, fnCallback) {
+			var oDialog = new Dialog({
+				title: 'Summary',
+				content: [
+				    new VBox({
+				        items: [
+				            new TextArea('summary', {
+    						    width: '100%',
+    						    value: sSummary,
+    						    placeholder: 'Input something...'
+					        })
+			            ]
+				    }).addStyleClass("sapUiTinyMargin")
+				],
+				beginButton: new Button({
+					type: ButtonType.Emphasized,
+					text: 'OK',
+					press: function () {
+						var sText = sap.ui.getCore().byId('summary').getValue();
+						fnCallback(null, sText);
+						oDialog.close();
+					}
+				}),
+				endButton: new Button({
+					text: 'Cancel',
+					press: function () {
+						oDialog.close();
+					}
+				}),
+				afterClose: function () {
+					oDialog.destroy();
+				}
+			});
+
+			oDialog.open();
+	    },
+	    
+		onDetailPress: function (oEvent) {
+		    var oBindingContext = oEvent.getSource().getBindingContext();
+		    var sPath = oBindingContext.getPath();
+		    var sSummary = oBindingContext.getProperty(sPath + "/summary");
+		    
+		    this._openDialog(sSummary, function (oError, sText) {
+		        oBindingContext.getModel().setProperty(sPath + "/summary", sText);
+		    });
+		},
+
 		onDrop: function (oEvent) {
 			var oModel = oEvent.getParameter("draggedControl").getBindingContext().getModel();
 			var oDraggedItem = oEvent.getParameter("draggedControl").getBindingContext().getObject();
